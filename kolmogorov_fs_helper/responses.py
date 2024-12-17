1ef959d0e02c607e1258fc933bbc4ab7f2463b49
Igor Rytsarev <rycarev@gmail.com>
2024-12-17 14:26:00 +0300

    update 1712

--- kolmogorov_fs_helper/responses.py
+++ kolmogorov_fs_helper/responses.py
@@ -1,12 +1,15 @@
 from fastapi import HTTPException, status
 from prometheus_client import Gauge
+import requests
 
 import pyspark
 
-def stream_parquet_rows(spark: pyspark.sql.SparkSession, file_path, active_requests_gauge: Gauge):
+def stream_parquet_rows(spark: pyspark.sql.SparkSession, file_path, METRICS_URL: str, username: str):
+        size_in_bytes = 0
         try:
             # Используем итератор по частям данных для чтения и отправки
             df = spark.read.parquet(file_path)
+            size_in_bytes = df.rdd.map(lambda row: len(str(row))).sum()
             for row in df.rdd.toLocalIterator():  # Прямой доступ к RDD
                 yield row.asDict().__str__().encode("utf-8") + b"\n"  # Преобразуем в строку JSON
         except Exception as e:
@@ -17,5 +20,11 @@ def stream_parquet_rows(spark: pyspark.sql.SparkSession, file_path, active_reque
                 )
         finally:
             # Завершение SparkSession после передачи данных
-            active_requests_gauge.dec()
+            # active_requests_gauge.dec()
+            try:
+                requests.post(f"{METRICS_URL}/received_historical_data", json={"value": size_in_bytes})
+                requests.post(f"{METRICS_URL}/active_requests", json={"value": -1})
+                requests.delete(f"{METRICS_URL}/active_users", json={"value": username})
+            except Exception:
+                print("stream_parquet_rows: METRICS SERVER NOT READY")
             spark.stop()
