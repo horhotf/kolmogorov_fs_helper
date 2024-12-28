e09dc140ecfadb3d19816fe75b6984ed2f966c51
Igor Rytsarev <irytsarev@vsk.ru>
2024-12-28 12:44:18 +0000

    upd 2812

--- kolmogorov_fs_helper/jupyter_helper.py
+++ kolmogorov_fs_helper/jupyter_helper.py
@@ -13,19 +13,26 @@ from typing import List, Dict
 
 
 class Helper:
-    def __init__(self, project_name:str, url = None):
-        self.project_name = project_name,
-        # self.url = f"http://feature-store-{project_name}-service.feast.svc.cluster.local:6566/cache"
+    def __init__(self, project_name:str, url: str = None):
+        self.project_name = project_name
+        
+        url_tmp = None
         if url == None:
-            self.url = "http://127.0.0.1:6566"
+            url_tmp = f"http://feature-store-{project_name}-service.feast.svc.cluster.local:6566"
         else:
-            self.url = url
+            url_tmp = url
+        
+        self.url = url_tmp
+        response = requests.get(self.url + "/health")
+        print(response)
 
     async def get_dataset(self, uid:str = None, feature_view:str = None, fields:str = None, filtred_params:str = None, time_limit:int = None):
         dataset = None
         limit = -1
 
         url = self.url + "/cache"
+        
+        print(url)
 
         if time_limit != None:
             limit = time_limit
@@ -42,7 +49,7 @@ class Helper:
             if filtred_params != None:
                 data["filtred_params"] = filtred_params
 
-            response = requests.get(url + "/cache", data=json.dumps(data))
+            response = requests.get(url, data=json.dumps(data))
             if response.status_code == 201:
                 res = json.loads(response.text)
                 uid = res["uid"]
@@ -58,7 +65,6 @@ class Helper:
                         elif response.status_code == 226:
                             current_ts = time.time()
                             diff_time = current_ts - start_ts
-                            print (diff_time)
                             if diff_time > limit:
                                 return f"Time limit (default 30 min) end. Rerty response with uid later. uid: {uid}"
                             else:
@@ -105,7 +111,6 @@ class Helper:
                     elif response.status_code == 226:
                         current_ts = time.time()
                         diff_time = current_ts - start_ts
-                        print (diff_time)
                         if diff_time > limit:
                             return f"Time limit (default 30 min) end. Rerty response with uid later. uid: {uid}"
                         else:
@@ -137,7 +142,7 @@ class Helper:
         else:
             return "Set one key: feature_view or uid"
 
-    async def get_on_demaind_features_df(self, features: list, entities: Dict[str, list[str]]):
+    def get_on_demaind_features_df(self, features: list, entities: Dict[str, list[str]]):
         url = self.url + "/get-online-features"
         
         data = {
@@ -158,10 +163,6 @@ class Helper:
                 print(f"Server error: {response.status_code} - {response.text}")
             else:
                 print(f"Unexpected error: {response.status}")
-        except aiohttp.ClientError as e:
-            print(f"Request failed: {e}")
-        except asyncio.TimeoutError:
-            print("Request timed out")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
             return None
@@ -170,7 +171,7 @@ class Helper:
         return pd.DataFrame(list(zip(*[result["values"] for result in json_data["results"]])), columns=json_data["metadata"]["feature_names"])
 
 
-    async def get_on_demaind_features_json(self, features: list, entities: Dict[str, list[str]]):
+    def get_on_demaind_features_json(self, features: list, entities: Dict[str, list[str]]):
         url = self.url + "/get-online-features"
         
         data = {
@@ -191,28 +192,9 @@ class Helper:
                 print(f"Server error: {response.status_code} - {response.text}")
             else:
                 print(f"Unexpected error: {response.status}")
-        except aiohttp.ClientError as e:
-            print(f"Request failed: {e}")
-        except asyncio.TimeoutError:
-            print("Request timed out")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
             return None
         
         json_data = json.loads(dataset)
         return dict(zip(json_data['metadata']['feature_names'], [result['values'][0] for result in json_data['results']]))
-
-
-
-
-# Пример использования
-# Создаем объект класса
-# fetcher = DataFetcher(name="MyFetcher")
-
-# # Выполняем фоновый запрос
-# url = 'https://example.com/data'
-# result = asyncio.create_task(fetcher.fetch_data_from_server(url))
-
-# После завершения result, результат будет доступен в fetcher.data
-# Для get_online_features:
-# response = await fetcher.get_online_features(query={"key": "value"}, url="https://example.com/api/features")
